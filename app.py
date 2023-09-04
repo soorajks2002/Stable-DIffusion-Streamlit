c3f476df1ca41aa85fd402e76a33fe43fa4e3146
sooraj <soorajks2002@gmail.com>
2023-09-04 21:22:11 +0530

    updated ui

--- app.py
+++ app.py
@@ -5,16 +5,19 @@ import stable_diffusion_xl_refiner_1
 from PIL import Image
 
 favicon = Image.open('favicon.ico')
-st.set_page_config(page_title="Stable Diffusion", page_icon=favicon)
+st.set_page_config(page_title="Stable Diffusion", page_icon=favicon, layout='wide')
 
 st.title("Stable Diffusion")
 
-form = st.form("Input Form")
+col1, col2 = st.columns(2)
+
+with col1:
+    form = st.form("Input Form")
 
 sd_models = ["Stable Diffusion 2", "Stable Diffusion XL Base 1", "Stable Diffusion Xl Refiner 1"]
 sd_model_map = {sd_models[0]:stable_diffusion_2, sd_models[1]:stable_diffusion_xl_base_1, sd_models[2]:stable_diffusion_xl_refiner_1}
 
-prompt = form.text_input("Enter prompt for image generation...")
+prompt = form.text_area('Enter prompt for image generation ... ', height=100, max_chars=150)
 model = form.radio("Select stable diffusion model", sd_models)
 
 if form.form_submit_button("Generate Image") :
@@ -22,9 +25,10 @@ if form.form_submit_button("Generate Image") :
         form.error("Please enter a valid prompt")
         
     else :
-        with st.spinner("Generating Image...") :
-            generated_output = sd_model_map[model].generate_image(prompt) 
-            if generated_output["generation_successful"] :
-                st.image(generated_output["image"])
-            else :
-                st.info(f"{model}'s Hugging Face Inference is BUSY !!")
+        with col2 :
+            with st.spinner("Generating Image...") :
+                generated_output = sd_model_map[model].generate_image(prompt) 
+                if generated_output["generation_successful"] :
+                    st.image(generated_output["image"])
+                else :
+                    st.info(f"{model}'s Hugging Face Inference is BUSY !!")
